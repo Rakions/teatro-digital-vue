d7984c06fa56475dd7882e23652a418acfc0b575
Omar <118843137+Rakions@users.noreply.github.com>
2024-02-20 19:52:57 +0100

    Empezar menu para dashboard

---
+++ src/components/Dashboard/MenuDashboard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <aside>
+        <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <path d="M50 0 L100 50 L50 100 L0 50 Z" fill="#964B00" />
+        </svg>
+
+
+    </aside>
+</template>
+
+<style scoped>
+aside {
+    width: 100px;
+    height: 100dvh;
+    background-color: bisque;
+}
+
+.logo {
+    width: 100%;
+}
+</style>
