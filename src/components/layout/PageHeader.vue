1b51e422934746a33f794c8a3bd4e35c61ff5476
Omar <118843137+Rakions@users.noreply.github.com>
2024-02-20 17:54:14 +0100

    Eliminar iconos sin usar

--- src/components/layout/PageHeader.vue
+++ src/components/layout/PageHeader.vue
@@ -4,14 +4,12 @@
       <ul class="header_ul">
         <li class="header_li">
           <div class="header_interestLinks">
-            <a href="/">{{ $t("message.funciones") }}</a>
-            <a href="notFound">{{ $t("message.ofertas") }}</a>
+            <a href="/">Funciones</a>
+            <a href="notFound">Ofertas</a>
           </div>
         </li>
         <li class="header_li">
-          <a href="/"
-            ><img src="../../assets/images/logos/teatro_galeguista_light.png" alt="Logo"
-          /></a>
+          <a href="/"><img src="../../assets/images/logos/teatro_galeguista_light.png" alt="Logo" /></a>
         </li>
         <li>
           <div class="header_information">
