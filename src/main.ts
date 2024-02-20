f4051c48964ae7436908d967011bec6eb49c350c
Omar <118843137+Rakions@users.noreply.github.com>
2024-02-20 19:24:06 +0100

    Instalar vuetify e iconos

--- src/main.ts
+++ src/main.ts
@@ -10,32 +10,41 @@ import App from './App.vue'
 import router from './router'
 import { createI18n } from 'vue-i18n'
 
-const messages = {
-  en: {
-    message: {
-      hello: 'hello world',
-      funciones: 'functions',
-      ofertas: 'sales'
-    }
-  },
-  ja: {
-    message: {
-      hello: 'こんにちは、世界',
-      funciones: '機能',
-      ofertas: 'オファー'
-    }
-  }
-}
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const i18n = createI18n({
-  locale: 'ja', // establece el idioma actual
-  fallbackLocale: 'en', // establece el idioma de respaldo
-  messages // establece los mensajes de idiomas
+const vuetify = createVuetify({
+  components,
+  directives
 })
+
+// const messages = {
+//   en: {
+//     message: {
+//       hello: 'hello world',
+//       funciones: 'functions',
+//       ofertas: 'sales'
+//     }
+//   },
+//   ja: {
+//     message: {
+//       hello: 'hola',
+//       funciones: 'funciones',
+//       ofertas: 'ofertas'
+//     }
+//   }
+// }
+
+// const i18n = createI18n({
+//   locale: 'es',
+//   fallbackLocale: 'en',
+//   messages
+// })
 
 const app = createApp(App)
 
 app.use(router)
-app.use(i18n)
 
-app.mount('#app')
+app.use(vuetify).mount('#app')
