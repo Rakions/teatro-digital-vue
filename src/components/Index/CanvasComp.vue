04fcef2c3edd3a2b5c26a138b9a778d9200d8c09
Omar <118843137+Rakions@users.noreply.github.com>
2024-03-14 19:33:42 +0100

    Crear componente del canvas

---
+++ src/components/Index/CanvasComp.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+interface Star {
+    x: number;
+    y: number;
+    size: number;
+    speed: number;
+}
+
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+let ctx: CanvasRenderingContext2D | null = null;
+
+const stars: Star[] = [];
+const numStars: number = 100;
+
+onMounted(() => {
+    if (canvasRef.value) {
+        const canvas = canvasRef.value;
+        ctx = canvas.getContext('2d');
+
+        for (let i = 0; i < numStars; i++) {
+            stars.push({
+                x: Math.random() * canvas.width,
+                y: Math.random() * canvas.height,
+                size: Math.random() * 2,
+                speed: Math.random() * 0.5 + 0.2
+            });
+        }
+
+        drawStars();
+    }
+});
+
+function drawStars(): void {
+
+    if (ctx && canvasRef.value) {
+
+        const canvas = canvasRef.value;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        stars.forEach(star => {
+            ctx.fillStyle = 'white';
+            ctx.beginPath();
+            ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+            ctx.fill();
+
+            star.y += star.speed;
+            if (star.y > canvas.height) {
+                star.y = 0;
+                star.x = Math.random() * canvas.width;
+            }
+        });
+
+        requestAnimationFrame(drawStars);
+    }
+}
+</script>
+
+<template>
+    <canvas class="canvasRef" ref="canvasRef" width="800" height="600"></canvas>
+</template>
+
+<style scoped>
+.canvasRef {
+    background-image: url("../../assets/images/mainPage_mainImage.jpg");
+    background-size: cover;
+}
+</style>
