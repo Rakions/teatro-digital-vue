205caaee1ddb1931e9a8027e722b03cb8b206282
Omar <118843137+Rakions@users.noreply.github.com>
2024-03-14 19:48:03 +0100

    Crear vista del register

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import PageHeader from '@/components/layout/PageHeader.vue';
+import PageFooter from '@/components/layout/PageFooter.vue';
+</script>
+
+<template>
+    <PageHeader />
+    <PageFooter />
+
+</template>
